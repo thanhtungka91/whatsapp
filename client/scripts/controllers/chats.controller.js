e413d4075d5b90f0f6a51b2e187d98e9967ce13f
Vo Thanh Tung <thanhtungka91@gmail.com>
2017-02-25 18:59:01 +0900

    add mongo

--- client/scripts/controllers/chats.controller.js
+++ client/scripts/controllers/chats.controller.js
@@ -1,5 +1,6 @@
 import Moment from 'moment';
 import { Controller } from 'angular-ecmascript/module-helpers';
+import { Chats } from '../../../lib/collections';
  
  // don gian cai nay chi la create db??
  // config template o da 
@@ -8,57 +9,18 @@ export default class ChatsCtrl extends Controller {
     super(...arguments);
     debugger;
     alert("here is debug");
-    this.data = [
-      {
-        _id: 0,
-        name: 'Ethan Gonzalez',
-        picture: 'https://randomuser.me/api/portraits/thumb/men/1.jpg',
-        lastMessage: {
-          text: 'You on your way?',
-          timestamp: Moment().subtract(1, 'hours').toDate()
-        }
-      },
-      {
-        _id: 1,
-        name: 'Bryan Wallace',
-        picture: 'https://randomuser.me/api/portraits/thumb/lego/1.jpg',
-        lastMessage: {
-          text: 'Hey, it\'s me',
-          timestamp: Moment().subtract(2, 'hours').toDate()
-        }
-      },
-      {
-        _id: 2,
-        name: 'Avery Stewart',
-        picture: 'https://randomuser.me/api/portraits/thumb/women/1.jpg',
-        lastMessage: {
-          text: 'I should buy a boat',
-          timestamp: Moment().subtract(1, 'days').toDate()
-        }
-      },
-      {
-        _id: 3,
-        name: 'Katie Peterson',
-        picture: 'https://randomuser.me/api/portraits/thumb/women/2.jpg',
-        lastMessage: {
-          text: 'Look at my mukluks!',
-          timestamp: Moment().subtract(4, 'days').toDate()
-        }
-      },
-      {
-        _id: 4,
-        name: 'Ray Edwards',
-        picture: 'https://randomuser.me/api/portraits/thumb/men/2.jpg',
-        lastMessage: {
-          text: 'This is wicked good ice cream.',
-          timestamp: Moment().subtract(2, 'weeks').toDate()
-        }
+
+    // create helper to get data from the server
+    // if you really spend time in project i believe you can do
+    this.helpers({
+      data() {
+        return Chats.find();
       }
-    ];
+    });
   }
 
   remove(chat) {
-    this.data.splice(this.data.indexOf(chat), 1);
+    Chats.remove(chat._id);
   }
 }
  
